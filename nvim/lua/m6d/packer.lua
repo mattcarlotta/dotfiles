5062679aa08738d02f92edb4d194d6d34547fc32
Matt <carlotta.matt@gmail.com>
2023-03-15 09:44:46 -0700

    chore: add gitsigns

--- nvim/lua/m6d/packer.lua
+++ nvim/lua/m6d/packer.lua
@@ -30,6 +30,11 @@ return require('packer').startup(function(use)
       end
   })
 
+  use {
+      'lewis6991/gitsigns.nvim',
+      -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+  }
+
   use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
   use("theprimeagen/harpoon")
   use("mbbill/undotree")
